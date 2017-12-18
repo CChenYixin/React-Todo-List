40f7d9585e298ed78504c0920c50b1b35d675d5a
sawako <onepiececee@163.com>
2017-12-18 18:55:28 +0800

    修改 css 文件

--- app/src/index.js
+++ app/src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<h1>Hi, world!</h1>, document.getElementById('root2'));
+ReactDOM.render(<h1>Hi, world!</h1>, document.getElementById('root'));
 registerServiceWorker();
