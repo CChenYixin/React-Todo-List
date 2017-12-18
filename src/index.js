e5177f5fe539f87e87b6136282251db3c23e7cb3
sawako <onepiececee@163.com>
2017-12-18 11:39:38 +0800

    引入jquery测试 webpack 第一个例子

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import _ from 'lodash';
+import $ from 'jquery';
 
 function component() {
-  var element = document.createElement('div');
+  var element = $('<div></div>');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.html(_.join(['Hello','lodash','and','jquery'],' '));
 
-  return element;
+  return element.get(0);
 }
 
 document.body.appendChild(component());
