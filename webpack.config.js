447d17230b6099b2484e4e13a5ac945758d83900
sawako <onepiececee@163.com>
2017-12-18 12:15:41 +0800

    测试 webpack 的压缩插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
-
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new UglifyJSPlugin({
+            // 最紧凑的输出
+            beautify: false,
+            // 删除所有注释
+            comments: false,
+            compress:{
+                // 在UglifyJs删除没有用到的代码时不输出警告
+                warnings: false
+            }
+        })
+    ]
 };
