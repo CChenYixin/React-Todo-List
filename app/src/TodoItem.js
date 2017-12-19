04e6cb638eaf3c2617af480cedcac91de650ab72
sawako <onepiececee@163.com>
2017-12-19 19:49:37 +0800

    添加删除按钮

--- app/src/TodoItem.js
+++ app/src/TodoItem.js
@@ -7,6 +7,7 @@ class TodoItem extends Component{
                 <input type='checkbox' checked={this.props.todo.status === 'completed'}
                 onChange={this.toggle.bind(this)} />
                 {this.props.todo.title}
+                <button onClick={this.delete.bind(this)}>删除</button>
             </div>
         )
     }
@@ -14,6 +15,11 @@ class TodoItem extends Component{
     toggle(e){
         this.props.onToggle(e,this.props.todo);
     }
+
+    delete(e){
+        this.props.onDelete(e, this.props.todo);
+    }
+
 }
 
 export default TodoItem;
