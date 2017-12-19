fb9d638ab276e96715e51d8d9d4c2bfe6c815364
sawako <onepiececee@163.com>
2017-12-19 14:11:23 +0800

    添加状态和生命周期钩子

--- app/src/Welcome.js
+++ app/src/Welcome.js
@@ -5,10 +5,34 @@ import React from 'react';
 //         return <h1> 这是自定义组件 </h1>;
 //     }
 // }
+class Welcome extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
 
-function Welcome(props){
-    return <h1> 这是自定义组件! </h1>;
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+
+    tick() {
+        this.setState({date: new Date()});
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
 }
-
 
 export default Welcome;
