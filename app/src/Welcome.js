802180d1273df05f484782bd52e863eaad06f5c3
sawako <onepiececee@163.com>
2017-12-19 13:37:26 +0800

    测试添加组件 component

---
+++ app/src/Welcome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+    render(){
+        return <h1> 这是自定义组件 </h1>;
+    }
+}
+
+export default Welcome;
--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Welcome from './Welcome'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<h1>Hi, world!</h1>, document.getElementById('root'));
+
+
+// function tick(){
+//     const element = (
+//         <div>
+//             <h1> hello world!</h1>
+//             <h2> it is {new Date().toLocaleString()}.</h2>
+//         </div>
+//     );
+//     ReactDOM.render(
+//         element,
+//         document.getElementById('root')
+//     );
+// }
+//
+// setInterval(tick,1000);
+
+ReactDOM.render(<Welcome/>, document.getElementById('root'));
 registerServiceWorker();
