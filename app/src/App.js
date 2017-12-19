f2fe439de29de49ca38c5cc57fbe68cd252ff7fd
sawako <onepiececee@163.com>
2017-12-19 15:05:31 +0800

    完成 App 的基本框架

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            newTodo:"测试",
+            todoList:[
+                {id:1,title:'第一个待办事项'}
+            ]
+        }
+    }
+
+
+
+    render() {
+
+        let todos = this.state.todoList.map((item,index)=>{
+            return <li>{item.title}</li>
+        })
+
+        return (
+            <div className = "App">
+                <h1>我的待办事项</h1>
+                <div className = "inputWrapper">
+                    <input type ="text" value={this.state.newTodo} />
+                </div>
+                <ol>
+                    {todos}
+                </ol>
+            </div>
+        );
+    }
 }
 
 export default App;
--- app/src/index.js
+++ app/src/index.js
@@ -2,25 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Welcome from './Welcome'
 import registerServiceWorker from './registerServiceWorker';
 
-
-
-// function tick(){
-//     const element = (
-//         <div>
-//             <h1> hello world!</h1>
-//             <h2> it is {new Date().toLocaleString()}.</h2>
-//         </div>
-//     );
-//     ReactDOM.render(
-//         element,
-//         document.getElementById('root')
-//     );
-// }
-//
-// setInterval(tick,1000);
-
-ReactDOM.render(<Welcome/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
