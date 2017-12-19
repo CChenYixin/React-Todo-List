4fb788d501b4ce6671da43ddc5ec71cd59120a9f
sawako <onepiececee@163.com>
2017-12-19 13:49:00 +0800

    props

--- app/src/Welcome.js
+++ app/src/Welcome.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-class Welcome extends React.Component {
-    render(){
-        return <h1> 这是自定义组件 </h1>;
-    }
+// class Welcome extends React.Component {
+//     render(){
+//         return <h1> 这是自定义组件 </h1>;
+//     }
+// }
+
+function Welcome(props){
+    return <h1> 这是自定义组件! </h1>;
 }
+
 
 export default Welcome;
