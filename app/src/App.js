89319bfcdb773a7698b18bde0779ecfbd868ef8a
sawako <onepiececee@163.com>
2017-12-19 19:33:39 +0800

    再次修复 value

--- app/src/App.js
+++ app/src/App.js
@@ -27,19 +27,26 @@ class App extends Component {
             )
         })
 
-        console.log(todos);
-
         return (
             <div className = "App">
                 <h1>我的待办事项</h1>
                 <div className = "inputWrapper">
-                    <TodoInput content = {this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
+                    <TodoInput content = {this.state.newTodo}
+                        onChange={this.changeTitle.bind(this)}
+                        onSubmit={this.addTodo.bind(this)} />
                 </div>
                 <ol>
                     {todos}
                 </ol>
             </div>
         );
+    }
+
+    changeTitle(event){
+        this.setState({
+            newTodo:event.target.value,
+            todoList:this.state.todoList
+        })
     }
 
     addTodo(event){
--- app/src/TodoInput.js
+++ app/src/TodoInput.js
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 class TodoInput extends Component{
     render(){
         return <input type ="text"
-            defaultValue={this.props.content}
+            value={this.props.content}
+            onChange={this.changeTitle.bind(this)}
             onKeyPress={this.submit.bind(this)}/>
     }
     submit(e){
         if(e.key === 'Enter'){
             this.props.onSubmit(e);
         }
+    }
+    changeTitle(e){
+        this.props.onChange(e);
     }
 
 }
