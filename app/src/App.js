b69934c9092b2cac3a50525661fce8f5ef7eb4a9
sawako <onepiececee@163.com>
2018-01-07 14:24:38 +0800

    登出功能

--- app/src/App.js
+++ app/src/App.js
@@ -5,87 +5,78 @@ import 'normalize.css';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
 import UserDialog from './UserDialog';
-import {getCurrentUser} from './leanCloud'
-
-
-
+import {getCurrentUser, signOut} from './leanCloud'
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             user: getCurrentUser() || {},
-            newTodo:'',
+            newTodo: '',
             todoList: []
         }
     }
 
     render() {
 
-        let todos = this.state.todoList
-            .filter((item)=> !item.deleted)
-            .map((item,index)=>{
+        let todos = this.state.todoList.filter((item) => !item.deleted).map((item, index) => {
             return (
                 <li key={index}>
-                    <TodoItem todo={item}
-                        onToggle={this.toggle.bind(this)}
-                        onDelete={this.delete.bind(this)} />
+                    <TodoItem todo={item} onToggle={this.toggle.bind(this)} onDelete={this.delete.bind(this)}/>
                 </li>
             )
         })
 
         return (
-            <div className = "App">
-                <h1>{this.state.user.username||'我'}的待办</h1>
-                <div className = "inputWrapper">
-                    <TodoInput content = {this.state.newTodo}
-                        onChange={this.changeTitle.bind(this)}
-                        onSubmit={this.addTodo.bind(this)} />
+            <div className="App">
+                <h1>{this.state.user.username || '我'}的待办 {this.state.user.id
+                        ? <button onClick={this.signOut.bind(this)}>登出</button>
+                        : null}
+                </h1>
+                <div className="inputWrapper">
+                    <TodoInput content={this.state.newTodo} onChange={this.changeTitle.bind(this)} onSubmit={this.addTodo.bind(this)}/>
                 </div>
                 <ol className="todoList">
                     {todos}
                 </ol>
-                {this.state.user.id ? null : <UserDialog onSignUp={this.onSignUp.bind(this)}/>}
+                {this.state.user.id
+                    ? null
+                    : <UserDialog onSignUp={this.onSignUp.bind(this)}/>}
             </div>
         );
     }
 
-    onSignUp(user){
+    signOut() {
+        signOut();
+        let stateCopy = JSON.parse(JSON.stringify(this.state));
+        stateCopy.user = {};
+        this.setState(stateCopy);
+    }
+    onSignUp(user) {
         let stateCopy = JSON.parse(JSON.stringify(this.state));
         stateCopy.user = user;
         this.setState(stateCopy);
-   }
-
-    componentDidUpdate(){
-
     }
 
-    toggle(e,todo){
-        todo.status = todo.status === 'completed' ? '': 'completed';
+    componentDidUpdate() {}
+
+    toggle(e, todo) {
+        todo.status = todo.status === 'completed'
+            ? ''
+            : 'completed';
         this.setState(this.state);
     }
 
-    changeTitle(event){
-        this.setState({
-            newTodo:event.target.value,
-            todoList:this.state.todoList
-        })
+    changeTitle(event) {
+        this.setState({newTodo: event.target.value, todoList: this.state.todoList})
     }
 
-    addTodo(event){
-        this.state.todoList.push({
-            id:idMaker(),
-            title:event.target.value,
-            status:null,
-            deleted:false
-        })
-        this.setState({
-            newTodo:'',
-            todoList:this.state.todoList
-        })
+    addTodo(event) {
+        this.state.todoList.push({id: idMaker(), title: event.target.value, status: null, deleted: false})
+        this.setState({newTodo: '', todoList: this.state.todoList})
     }
 
-    delete(event,todo){
+    delete(event, todo) {
         todo.deleted = true;
         this.setState(this.state);
     }
@@ -94,9 +85,9 @@ class App extends Component {
 
 export default App;
 
-let id =0;
+let id = 0;
 
-function idMaker(){
-    id +=1;
+function idMaker() {
+    id += 1;
     return id;
 }
