1b3b87ef286d1148c441148e284c21f75c8f164a
sawako <onepiececee@163.com>
2018-01-08 14:59:06 +0800

    添加找回密码功能

--- app/src/UserDialog.js
+++ app/src/UserDialog.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './UserDialog.css';
-import {signUp,signIn} from './leanCloud';
+import {signUp, signIn, sendPasswordResetEmail} from './leanCloud';
 
 export default class UserDialog extends Component {
 
@@ -8,7 +8,9 @@ export default class UserDialog extends Component {
         super(props);
         this.state = {
             selected: 'signUp',
+            selectedTab: 'signInOrSignUp',
             formData: {
+                email: '',
                 username: '',
                 password: ''
             }
@@ -21,12 +23,12 @@ export default class UserDialog extends Component {
 
     signUp(e) {
         e.preventDefault();
-        let {username, password} = this.state.formData;
+        let {email, username, password} = this.state.formData;
         let success = (user) => {
             this.props.onSignUp.call(null, user)
         };
         let error = (error) => {
-            switch(error.code){
+            switch (error.code) {
                 case 202:
                     alert('用户名已被占用');
                     break;
@@ -35,18 +37,18 @@ export default class UserDialog extends Component {
                     break;
             }
         };
-        signUp(username, password, success, error);
+        signUp(email, username, password, success, error);
     }
 
     signIn(e) {
         e.preventDefault();
         let {username, password} = this.state.formData;
-        let success = (user)=>{
+        let success = (user) => {
             this.props.onSignIn.call(null, user)
         };
 
-        let error = (error)=>{
-            switch(error.code){
+        let error = (error) => {
+            switch (error.code) {
                 case 210:
                     alert('用户名与密码不匹配');
                     break;
@@ -68,6 +70,10 @@ export default class UserDialog extends Component {
         let signUpForm = (
             <form className="signUp" onSubmit={this.signUp.bind(this)}>
                 {/* 注册*/}
+                <div className="row">
+                    <label>邮箱</label>
+                    <input type="text" value={this.state.formData.email} onChange={this.changeFormData.bind(this, 'email')}/>
+                </div>
                 <div className="row">
                     <label>用户名</label>
                     <input type="text" value={this.state.formData.username} onChange={this.changeFormData.bind(this, 'username')}/>
@@ -94,30 +100,73 @@ export default class UserDialog extends Component {
                 </div>
                 <div className="row actions">
                     <button type="submit">登录</button>
+                    <a href="#" onClick={this.showForgotPassword.bind(this)}>忘记密码了？</a>
                 </div>
             </form>
+        )
+        let signInOrSignUp = (
+            <div className="signInOrSignUp">
+                <nav>
+                    <label>
+                        <input type="radio" value="signUp" checked={this.state.selected === 'signUp'} onChange={this.switch.bind(this)}/>
+                        注册</label>
+                    <label>
+                        <input type="radio" value="signIn" checked={this.state.selected === 'signIn'} onChange={this.switch.bind(this)}/>
+                        登录</label>
+                </nav>
+                <div className="panes">
+                    {this.state.selected === 'signUp'
+                        ? signUpForm
+                        : null}
+                    {this.state.selected === 'signIn'
+                        ? signInForm
+                        : null}
+                </div>
+
+            </div>
+        )
+        let forgotPassword = (
+            <div className="forgotPassword">
+                <h3>
+                    重置密码
+                </h3>
+                <form className="forgotPassword" onSubmit={this.resetPassword.bind(this)}>
+                    {/* 登录*/}
+                    <div className="row">
+                        <label>邮箱</label>
+                        <input type="text" value={this.state.formData.email} onChange={this.changeFormData.bind(this, 'email')}/>
+                    </div>
+                    <div className="row actions">
+                        <button type="submit">发送重置邮件</button>
+                        <a href="#" onClick={this.returnToSignIn.bind(this)}>返回登录</a>
+                    </div>
+                </form>
+            </div>
         )
         return (
             <div className="UserDialog-Wrapper">
                 <div className="UserDialog">
-                    <nav>
-                        <label>
-                            <input type="radio" value="signUp" checked={this.state.selected === 'signUp'} onChange={this.switch.bind(this)}/>
-                            注册</label>
-                        <label>
-                            <input type="radio" value="signIn" checked={this.state.selected === 'signIn'} onChange={this.switch.bind(this)}/>
-                            登录</label>
-                    </nav>
-                    <div className="panes">
-                        {this.state.selected === 'signUp'
-                            ? signUpForm
-                            : null}
-                        {this.state.selected === 'signIn'
-                            ? signInForm
-                            : null}
-                    </div>
+                    {this.state.selectedTab === 'signInOrSignUp'
+                        ? signInOrSignUp
+                        : forgotPassword}
                 </div>
             </div>
         )
     }
+
+    showForgotPassword() {
+        let stateCopy = JSON.parse(JSON.stringify(this.state))
+        stateCopy.selectedTab = 'forgotPassword'
+        this.setState(stateCopy)
+    }
+    returnToSignIn() {
+        let stateCopy = JSON.parse(JSON.stringify(this.state))
+        stateCopy.selectedTab = 'signInOrSignUp'
+        this.setState(stateCopy)
+    }
+    resetPassword(e) {
+        e.preventDefault()
+        sendPasswordResetEmail(this.state.formData.email)
+    }
+
 }
